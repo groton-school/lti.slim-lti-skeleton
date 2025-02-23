e36b943a74b22cb248d7fb32edfb3b687e6fb3d8
battis <sbattis@groton.org>
2025-02-23 20:26:28 +0000

    refactor: prettier composer.json

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,23 +1,21 @@
 import gcloud from '@battis/partly-gcloudy';
-import cli from '@battis/qui-cli';
+import { Core } from '@battis/qui-cli.core';
+import { Shell } from '@battis/qui-cli.shell';
 import path from 'node:path';
 
 (async () => {
-  const {
-    values: { force }
-  } = await gcloud.init({
-    env: {
-      root: path.dirname(import.meta.dirname)
-    },
-    args: {
-      flags: {
-        force: {
-          short: 'f',
-          default: false
-        }
+  Core.configure({ root: { root: path.dirname(import.meta.dirname) } });
+  const args = await Core.init({
+    flag: {
+      force: {
+        short: 'f',
+        default: false
       }
     }
   });
+  const {
+    values: { force }
+  } = args;
 
   if (process.env.PROJECT && !force) {
     await gcloud.batch.appEngineDeployAndCleanup({ retainVersions: 2 });
@@ -26,11 +24,11 @@ import path from 'node:path';
 
     await gcloud.services.enable(gcloud.services.API.CloudFirestoreAPI);
     const [{ name: database }] = JSON.parse(
-      cli.shell.exec(
+      Shell.exec(
         `gcloud firestore databases list --project=${project.projectId} --format=json --quiet`
       )
     );
-    cli.shell.exec(
+    Shell.exec(
       `gcloud firestore databases update --type=firestore-native --database="${database}" --project=${project.projectId} --format=json --quiet`
     );
 
